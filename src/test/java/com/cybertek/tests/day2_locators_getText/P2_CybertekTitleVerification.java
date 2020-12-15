42f809b029027d8a054bc1470a4287109d3d99cb
gurhank <gurhankocyigit@gmail.com>
2020-12-15 10:57:12 -0600

    day2_commits practice 2

--- src/test/java/com/cybertek/tests/day2_locators_getText/P2_CybertekTitleVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText/P2_CybertekTitleVerification.java
@@ -23,15 +23,26 @@ public class P2_CybertekTitleVerification {
         String expectedInUrl = "cybertekschool";
         String actualUrl =  driver.getCurrentUrl();
 
+        System.out.println("expectedInUrl = " + expectedInUrl);
+        System.out.println("actualUrl = " + actualUrl);
+
         if (actualUrl.contains(expectedInUrl)){
             System.out.println("URL verification PASSED!");
         }else{
-            System.out.println("URL verification FAILED!!!");
+            System.err.println("URL verification FAILED!!!");
         }
-
 
         //4. Verify title:
         //Expected: Practice
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASSED!!!");
+        }else{
+            System.err.println("Title verification FAILED!!!");
+        }
+
 
     }
 }
