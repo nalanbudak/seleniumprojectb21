e4e7e9686c7f2d0eca9ae7d34a37fcc9bc7bc886
gurhank <gurhankocyigit@gmail.com>
2020-12-15 10:43:09 -0600

    day2_commits practice 2

--- src/test/java/com/cybertek/tests/day2_locators_getText/P2_CybertekTitleVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText/P2_CybertekTitleVerification.java
@@ -1,15 +1,26 @@
 package com.cybertek.tests.day2_locators_getText;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P2_CybertekTitleVerification {
 
     public static void main(String[] args) {
 
         //TC #2: Cybertek verifications
         //1. Open Chrome browser
+        //setup my driver
+        WebDriverManager.chromedriver().setup();
+        //open browser
+        WebDriver driver = new ChromeDriver();
+
         //2. Go to https://practice.cybertekschool.com
+        driver.get("https://practice.cybertekschool.com");
+
         //3. Verify URL contains
         //Expected: cybertekschool
-        // 4. Verify title:
+        //4. Verify title:
         //Expected: Practice
 
     }
