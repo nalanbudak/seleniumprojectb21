3ed92b633ce88baa0e1a511183870bf35621340c
gurhank <gurhankocyigit@gmail.com>
2020-12-15 10:15:01 -0600

    day2_commits

---
+++ src/test/java/com/cybertek/tests/day2_locators_getText/P1_FacebookTitleVerification.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day2_locators_getText;
+
+public class P1_FacebookTitleVerification {
+
+    public static void main(String[] args) {
+
+        //TC #1: Facebook Title Verification
+        //1. Open Chrome browser
+        //2. Go to https://www.facebook.com
+        //3. Verify title:
+        //Expected: Facebook - Log In or Sign Up
+
+
+    }
+
+}
