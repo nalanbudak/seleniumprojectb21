ddad933016731e54b384cacb66effd346e6922c5
gurhank <gurhankocyigit@gmail.com>
2020-12-22 11:57:07 -0600

    day3_commits warm up task solution

---
+++ src/test/java/com/cybertek/utilities/WebDriverFactory.java
@@ -0,0 +1,15 @@
+package com.cybertek.utilities;
+
+public class WebDriverFactory {
+
+    //TASK: NEW METHOD CREATION
+    //   Method name: getDriver
+    //   Static method
+    //   Accepts string argument: browserType --> will determine what browser opens
+    //       -> if "chrome" opens chrome browser
+    //       -> if "firefox" opens firefox browser
+    //   return type: WebDriver
+
+    //EXTRA 15 MINS FOR LUNCH FOR THE TASK
+
+}
