45b2ced28185ddd774f26fd9871dc30826e65823
gurhank <gurhankocyigit@gmail.com>
2020-12-22 10:16:16 -0600

    day3_commits warm up task solution

---
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
@@ -0,0 +1,21 @@
+package com.cybertek.tests.day3_cssSelector_xpath;
+
+public class WT_CybertekLinkVerification {
+
+    public static void main(String[] args) {
+
+        //TC #3: PracticeCybertek/ForgotPassword URL verification
+        //1. Open Chrome browser
+        //2. Go to http://practice.cybertekschool.com/forgot_password
+        //3. Enter any email into input box
+        //4. Click on Retrieve password
+        //5. Verify URL contains:
+        //Expected: “email_sent”
+        //6. Verify textbox displayed the content as expected.
+        //Expected: “Your e-mail’s been sent!”
+
+
+
+    }
+
+}
