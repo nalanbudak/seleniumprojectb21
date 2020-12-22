acf64a9a826804e71600eba22fb6951ab8a9666c
gurhank <gurhankocyigit@gmail.com>
2020-12-22 14:38:43 -0600

    day3_commits WebDriverFactory utility method

---
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P3_Amazon_cssSelectorTask.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day3_cssSelector_xpath;
+
+public class P3_Amazon_cssSelectorTask {
+
+    public static void main(String[] args) {
+
+        //1. Open Chrome browser
+        //2. Go to https://www.amazon.com
+        //3. Enter search term (use cssSelector to locate search box)
+        //4. Verify title contains search term
+
+
+    }
+
+}
