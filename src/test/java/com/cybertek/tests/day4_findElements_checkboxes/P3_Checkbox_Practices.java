addbcf0a7781de15b71336c0b9f8c5c2d6bed1fd
gurhank <gurhankocyigit@gmail.com>
2021-01-04 15:17:52 -0600

    day4_commits find elements practice

---
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P3_Checkbox_Practices.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day4_findElements_checkboxes;
+
+public class P3_Checkbox_Practices {
+
+    public static void main(String[] args) {
+        //Practice: Cybertek Checkboxes
+        //1. Go to http://practice.cybertekschool.com/checkboxes
+        //2. Confirm checkbox #1 is NOT selected by default
+        //3. Confirm checkbox #2 is SELECTED by default.
+        //4. Click checkbox #1 to select it.
+        //5. Click checkbox #2 to deselect it.
+        //6. Confirm checkbox #1 is SELECTED.
+        //7. Confirm checkbox #2 is NOT selected.
+    }
+}
