3b3cb78e7063daa108401c5effdba6662e173e9b
gurhank <gurhankocyigit@gmail.com>
2021-01-04 12:06:28 -0600

    day4_commits xpath locator practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P1_Xpath_CssSelector.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P1_Xpath_CssSelector.java
@@ -54,7 +54,15 @@ public class P1_Xpath_CssSelector {
 
         //f. “Powered by Cybertek School” text
         WebElement footerText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
-        
+
         //4. Verify all WebElements are displayed.
+
+        if(homeLink.isDisplayed() && forgotPasswordHeader.isDisplayed()
+        && emailLabel.isDisplayed() && emailInputBox.isDisplayed()
+        && retrievePasswordButton.isDisplayed() && footerText.isDisplayed() ){
+            System.out.println("ALL WEB ELEMENTS ARE DISPLAYED. VERIFICATION PASSED!");
+        }else{
+            System.out.println("One or more of the web elements are not displayed. Verification FAILED!!!");
+        }
     }
 }
