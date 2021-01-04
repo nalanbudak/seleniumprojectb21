58386c8a08e658ef6bdb9cfd93a644feb9fc90dc
gurhank <gurhankocyigit@gmail.com>
2021-01-04 14:16:09 -0600

    day4_commits find elements practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P0_FindElements_Practice1.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P0_FindElements_Practice1.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class P0_FindElements_Practice1 {
 
@@ -17,16 +18,24 @@ public class P0_FindElements_Practice1 {
         // go to: http://practice.cybertekschool.com/forgot_password
         driver.get("http://practice.cybertekschool.com/forgot_password");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         //#2- Locate all of the LINKS on the page, and print out all the texts
         // first, we need to find the locator that is returning us all of the links in the page
         //body//a
 
         // second, store all of the links in to a List of WebElements
-        
+
         List<WebElement> listOfLinks = driver.findElements(By.xpath("//body//a"));
 
-            // third, create java logic to print out all of the texts of all links
+        // third, create java logic to print out all of the texts of all links
+
+        for(WebElement eachLink : listOfLinks){
+
+            System.out.println(eachLink.getText());
+
+        }
+
 
         //#3- Print out how many links on the page
 
