c64cb5b34c2701a5638bf82023df6d519aae17a5
gurhank <gurhankocyigit@gmail.com>
2021-01-04 15:39:03 -0600

    day4_commits find elements practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P3_Checkbox_Practices.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P3_Checkbox_Practices.java
@@ -17,13 +17,23 @@ public class P3_Checkbox_Practices {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-        //2. Confirm checkbox #1 is NOT selected by default
+        //LOCATING CHECKBOXES
         WebElement checkbox1 = driver.findElement(By.xpath("//input[@type='checkbox'][1]"));
         WebElement checkbox2 = driver.findElement(By.xpath("//input[@type='checkbox'][2]"));
 
+        // another option as locator for checkboxes: //form[@id='checkboxes']/input[1]
+        // another option as locator for checkboxes: //form[@id='checkboxes']/input[2]
 
+        //2. Confirm checkbox #1 is NOT selected by default
+        if(!checkbox1.isSelected()){
+            System.out.println("Checkbox#1 is NOT selected. Default value verification PASSED! ");
+        }else{
+            System.out.println("Checkbox#1 is selected. Default value verification FAILED!!!");
+        }
 
         //3. Confirm checkbox #2 is SELECTED by default.
+
+
         //4. Click checkbox #1 to select it.
         //5. Click checkbox #2 to deselect it.
         //6. Confirm checkbox #1 is SELECTED.
