298a9f4516e3944b7b4ffb9f18e02021e37c30d2
gurhank <gurhankocyigit@gmail.com>
2021-01-04 14:02:00 -0600

    day4_commits find elements practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P0_FindElements_Practice1.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P0_FindElements_Practice1.java
@@ -1,12 +1,22 @@
 package com.cybertek.tests.day4_findElements_checkboxes;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
 public class P0_FindElements_Practice1 {
 
     public static void main(String[] args) {
 
-        //#1- Open chrome browser and go to: http://practice.cybertekschool.com/forgot_password
+        //#1- Open chrome browser and
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        // go to: http://practice.cybertekschool.com/forgot_password
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        driver.manage().window().maximize();
+
         //#2- Locate all of the LINKS on the page, and print out all the texts
         //#3- Print out how many links on the page
+
 
     }
 }
