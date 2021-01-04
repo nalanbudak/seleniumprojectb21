8de903d3a1f0876f0f9c0374e46929ca997f15cf
gurhank <gurhankocyigit@gmail.com>
2021-01-04 15:29:22 -0600

    day4_commits find elements practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P3_Checkbox_Practices.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P3_Checkbox_Practices.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day4_findElements_checkboxes;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +18,11 @@ public class P3_Checkbox_Practices {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         //2. Confirm checkbox #1 is NOT selected by default
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[@type='checkbox'][1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//input[@type='checkbox'][2]"));
+
+
+
         //3. Confirm checkbox #2 is SELECTED by default.
         //4. Click checkbox #1 to select it.
         //5. Click checkbox #2 to deselect it.
