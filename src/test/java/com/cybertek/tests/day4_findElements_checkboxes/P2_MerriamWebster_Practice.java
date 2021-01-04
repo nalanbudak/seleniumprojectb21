a180e93969f51ad3470ed6d555bcd9dcfe4858e1
gurhank <gurhankocyigit@gmail.com>
2021-01-04 14:26:57 -0600

    day4_commits find elements practice

---
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P2_MerriamWebster_Practice.java
@@ -0,0 +1,14 @@
+package com.cybertek.tests.day4_findElements_checkboxes;
+
+public class P2_MerriamWebster_Practice {
+
+    public static void main(String[] args) {
+        //1. Open Chrome browser
+        //2. Go to https://www.merriam-webster.com/
+        //3. Print out the texts of all links
+        //4. Print out how many link is missing text
+        //5. Print out how many link has text
+        //6. Print out how many total link
+    }
+
+}
