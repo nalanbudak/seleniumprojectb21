b48ab04302bcd0a102669ffd9dea45c468fa0a85
gurhank <gurhankocyigit@gmail.com>
2021-01-04 11:15:23 -0600

    day4_commits xpath locator practice

---
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P1_Xpath_CssSelector.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day4_findElements_checkboxes;
+
+public class P1_Xpath_CssSelector {
+
+    public static void main(String[] args) {
+        //TC #1: PracticeCybertek.com_ForgotPassword WebElement verification
+        //1. Open Chrome browser
+        //2. Go to http://practice.cybertekschool.com/forgot_password
+        //3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
+        //a. “Home” link
+        //b. “Forgot password” header
+        //c. “E-mail” text
+        //d. E-mail input box
+        //e. “Retrieve password” button
+        //f. “Powered by Cybertek School” text
+        //4. Verify all WebElements are displayed.
+    }
+}
