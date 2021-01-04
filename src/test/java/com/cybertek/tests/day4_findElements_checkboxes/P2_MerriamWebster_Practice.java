14798354fbdf3a05cb2a35af98de17c7b5d8e9df
gurhank <gurhankocyigit@gmail.com>
2021-01-04 14:28:53 -0600

    day4_commits find elements practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P2_MerriamWebster_Practice.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P2_MerriamWebster_Practice.java
@@ -1,10 +1,17 @@
 package com.cybertek.tests.day4_findElements_checkboxes;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
 public class P2_MerriamWebster_Practice {
 
     public static void main(String[] args) {
         //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
         //2. Go to https://www.merriam-webster.com/
+        driver.get("https://www.merriam-webster.com/");
+
         //3. Print out the texts of all links
         //4. Print out how many link is missing text
         //5. Print out how many link has text
