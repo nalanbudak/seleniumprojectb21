00b4a762ba0ba9c934e10910d6811bfd9533d69f
gurhank <gurhankocyigit@gmail.com>
2021-01-08 14:35:11 -0600

    day5_commits checkbox practice

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,27 +1,35 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Introduction {
 
+    @BeforeMethod
+    public void setupMethod(){
+        System.out.println("Before method is running...");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("After method is running...");
+    }
+
+
     /*
-    @BeforeClass will make the method run once before everything else in the class.
-     */
-   @BeforeClass
-   public void setupClass(){
-       System.out.println("Before class is running...");
-   }
+      @BeforeClass will make the method run once before everything else in the class.
+       */
+    @BeforeClass
+    public void setupClass(){
+        System.out.println("Before class is running...");
+    }
 
     /*
      @AfterClass will make the method run once after everything else in the class.
       */
-   @AfterClass
-   public void teardownClass(){
-       System.out.println("After class is running...");
-   }
-
+    @AfterClass
+    public void teardownClass(){
+        System.out.println("After class is running...");
+    }
 
    @Test
    public void test1(){
@@ -32,5 +40,7 @@ public class TestNG_Introduction {
    public void test2(){
        System.out.println("Running test 2...");
    }
+
+
 
 }
