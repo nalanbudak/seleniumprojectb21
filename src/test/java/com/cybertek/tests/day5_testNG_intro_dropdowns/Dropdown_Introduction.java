555e275660b4e16298337d457460ec977972a1fe
gurhank <gurhankocyigit@gmail.com>
2021-01-08 15:44:57 -0600

    day5_commits dropdowns

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/Dropdown_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/Dropdown_Introduction.java
@@ -1,6 +1,11 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Dropdown_Introduction {
 
@@ -11,6 +16,15 @@ public class Dropdown_Introduction {
     //Expected: "Please select an option"
     //4. Verify "State selection" default selected value is correct
     //Expected: "Select a State"
+
+    @BeforeClass
+    public void setupClass(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.get("http://practice.cybertekschool.com/dropdown");
+    }
 
     @Test
     public void dropdown_test1(){
