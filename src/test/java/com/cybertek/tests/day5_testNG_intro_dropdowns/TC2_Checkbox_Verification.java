babd8bc25a67ff390d701e72a967daabf9308ac1
gurhank <gurhankocyigit@gmail.com>
2021-01-08 10:20:23 -0600

    day5_commits checkbox practice

---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day5_testNG_intro_dropdowns;
+
+public class TC2_Checkbox_Verification {
+
+    public static void main(String[] args) {
+        //1. Open Chrome browser
+        //2. Go to https://www.seleniumeasy.com/test/basic-checkbox-demo.html
+        //3. Verify “Success – Check box is checked” message is NOT displayed.
+        //4. Click to checkbox under “Single Checkbox Demo” section
+        //5. Verify “Success – Check box is checked” message is displayed.
+    }
+}
