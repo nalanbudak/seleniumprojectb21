bbcb3efdfe5c7e406ec3e80bc9bb77119d22c081
gurhank <gurhankocyigit@gmail.com>
2021-01-08 13:54:11 -0600

    day5_commits checkbox practice

---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Introduction {
+
+   @Test
+   public void test1(){
+       System.out.println("Running test 1...");
+   }
+
+   @Test
+   public void test2(){
+       System.out.println("Running test 2...");
+   }
+
+}
