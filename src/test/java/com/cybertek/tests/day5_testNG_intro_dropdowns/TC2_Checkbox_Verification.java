d1a6dd8cbd4d7f87ab9ac6055512f09bbd357575
gurhank <gurhankocyigit@gmail.com>
2021-01-08 10:52:21 -0600

    day5_commits checkbox practice

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,9 +22,16 @@ public class TC2_Checkbox_Verification {
         //creating implicit wait for our findElement method
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-
-
         //3. Verify “Success – Check box is checked” message is NOT displayed.
+
+        //Option #1: Using xpath and id attribute value to locate successMessage
+        WebElement successMessage = driver.findElement(By.xpath("//div[@id='txtAge']"));
+        //Option #2: Using xpath and style attribute value: //div[@style='display: block;']
+        //Option #3: Using xpath and text value: //div[.='Success - Check box is checked']
+        //Option #3: Using xpath and text value: //div[text()='Success - Check box is checked']
+
+
+
         //4. Click to checkbox under “Single Checkbox Demo” section
         //5. Verify “Success – Check box is checked” message is displayed.
     }
