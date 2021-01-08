9d6d43334f5ae4fbedda11fda612ed0bca74638a
gurhank <gurhankocyigit@gmail.com>
2021-01-08 10:27:52 -0600

    day5_commits checkbox practice

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
@@ -1,10 +1,17 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
 public class TC2_Checkbox_Verification {
 
     public static void main(String[] args) {
         //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
         //2. Go to https://www.seleniumeasy.com/test/basic-checkbox-demo.html
+        driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+
         //3. Verify “Success – Check box is checked” message is NOT displayed.
         //4. Click to checkbox under “Single Checkbox Demo” section
         //5. Verify “Success – Check box is checked” message is displayed.
