50eb02f65db273a449e04ccdf513514bd33a76eb
gurhank <gurhankocyigit@gmail.com>
2021-01-08 14:57:45 -0600

    day5_commits checkbox practice

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,17 +1,18 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class TestNG_Introduction {
 
     @BeforeMethod
     public void setupMethod(){
-        System.out.println("Before method is running...");
+        System.out.println("--->Before method is running...");
     }
 
     @AfterMethod
     public void tearDownMethod(){
-        System.out.println("After method is running...");
+        System.out.println("--->After method is running...");
     }
 
 
@@ -20,7 +21,7 @@ public class TestNG_Introduction {
        */
     @BeforeClass
     public void setupClass(){
-        System.out.println("Before class is running...");
+        System.out.println("------>Before class is running...");
     }
 
     /*
@@ -28,17 +29,34 @@ public class TestNG_Introduction {
       */
     @AfterClass
     public void teardownClass(){
-        System.out.println("After class is running...");
+        System.out.println("------>After class is running...");
     }
 
    @Test
    public void test1(){
-       System.out.println("Running test 1...");
+       System.out.println("->Running test 1...");
    }
 
    @Test
    public void test2(){
-       System.out.println("Running test 2...");
+       System.out.println("->Running test 2...");
+   }
+
+   @Test
+   public void test3(){
+       System.out.println("->Running test 3...");
+
+       String str1 = "hello";
+       String str2 = "hello2asdfasdf";
+
+       //assertTrue: expects a boolean value to be passed
+       Assert.assertTrue(str1.equals(str2) , "Comparing hello word's failed.");
+
+       //assertEquals: expects two arguments of the same type: String VS String etc.
+       Assert.assertEquals(str1, str2 , "Asserting with assertEquals on line 56");
+
+       //the last string argument will be accepted as "failure message"
+
    }
 
 
