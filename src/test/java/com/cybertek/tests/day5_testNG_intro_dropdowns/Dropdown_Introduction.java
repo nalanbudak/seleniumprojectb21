cd433f1e95e12c41269bc5189a6ab6983d553b6b
gurhank <gurhankocyigit@gmail.com>
2021-01-08 15:41:02 -0600

    day5_commits dropdowns

---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/Dropdown_Introduction.java
@@ -0,0 +1,20 @@
+package com.cybertek.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class Dropdown_Introduction {
+
+    //TC#1: Verifying "Simple dropdown" and "State selection" dropdown default values
+    //1. Open Chrome browser
+    //2. Go to http://practice.cybertekschool.com/dropdown
+    //3. Verify "Simple dropdown" default selected value is correct
+    //Expected: "Please select an option"
+    //4. Verify "State selection" default selected value is correct
+    //Expected: "Select a State"
+
+    @Test
+    public void dropdown_test1(){
+
+    }
+
+}
