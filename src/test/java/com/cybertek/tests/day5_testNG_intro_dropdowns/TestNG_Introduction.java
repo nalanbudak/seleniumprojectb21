5c1d01f622856ab9be316a36b806b7d5ea159c2f
gurhank <gurhankocyigit@gmail.com>
2021-01-08 14:19:43 -0600

    day5_commits checkbox practice

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,8 +1,21 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestNG_Introduction {
+
+   @BeforeClass
+   public void setupClass(){
+       System.out.println("Before class is running...");
+   }
+
+   @AfterClass
+   public void teardownClass(){
+       System.out.println("After class is running...");
+   }
+
 
    @Test
    public void test1(){
