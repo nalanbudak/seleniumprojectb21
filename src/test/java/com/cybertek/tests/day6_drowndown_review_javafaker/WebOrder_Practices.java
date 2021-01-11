a5b5cefcae6e27be4f6488c3a332acb899df9ba5
gurhank <gurhankocyigit@gmail.com>
2021-01-11 14:08:11 -0600

    day6_commits web_order practices

---
+++ src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/WebOrder_Practices.java
@@ -0,0 +1,41 @@
+package com.cybertek.tests.day6_drowndown_review_javafaker;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebOrder_Practices {
+
+    //3. Enter username: “Tester”
+    //4. Enter password: “test”
+    //5. Click to Login button
+    //6. Print out count of all the links on landing page
+    //7. Print out each link text on this page
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //1. Open browser
+        //2. Go to website:
+        //http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+        driver = WebDriverFactory.getDriver("chrome");
+
+        //2. Go to http://practice.cybertekschool.com/dropdown
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        //maximize
+        driver.manage().window().maximize();
+
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void test1_link_verifications(){
+
+    }
+}
