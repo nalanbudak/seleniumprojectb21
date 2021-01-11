01acfc30053f920412e7a10c10219110633a1839
gurhank <gurhankocyigit@gmail.com>
2021-01-11 14:13:48 -0600

    day6_commits web_order practices

--- src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/WebOrder_Practices.java
+++ src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/WebOrder_Practices.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day6_drowndown_review_javafaker;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -33,8 +35,18 @@ public class WebOrder_Practices {
     @Test
     public void test1_link_verifications(){
         //3. Enter username: “Tester”
+        WebElement inputUsername = driver.findElement(By.id("ctl00_MainContent_username"));
+        inputUsername.sendKeys("Tester");
+
         //4. Enter password: “test”
+        WebElement inputPassword = driver.findElement(By.id("ctl00_MainContent_password"));
+        inputPassword.sendKeys("test");
+
         //5. Click to Login button
+        WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+        loginButton.click();
+
+
         //6. Print out count of all the links on landing page
         //7. Print out each link text on this page
     }
