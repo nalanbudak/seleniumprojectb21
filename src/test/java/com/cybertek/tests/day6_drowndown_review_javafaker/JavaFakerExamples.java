b0b9a35b277edc26785cdfaa5e39318e0abc95af
gurhank <gurhankocyigit@gmail.com>
2021-01-11 13:30:46 -0600

    day6_commits dropdown practices

--- src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/JavaFakerExamples.java
+++ src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/JavaFakerExamples.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day6_drowndown_review_javafaker;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
 public class JavaFakerExamples {
@@ -7,6 +8,18 @@ public class JavaFakerExamples {
 
     @Test
     public void java_faker_test1(){
+        //We need to create an object of the JavaFaker
+        Faker faker = new Faker();
+
+        String firstName = faker.name().firstName();
+        System.out.println("firstName = " + firstName);
+
+        String lastName = faker.name().lastName();
+        System.out.println("lastName = " + lastName);
+
+        String fullName = faker.name().fullName();
+        System.out.println("fullName = " + fullName);
+
 
     }
 }
