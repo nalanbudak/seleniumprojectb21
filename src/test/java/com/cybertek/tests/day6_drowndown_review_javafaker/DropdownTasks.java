92639a6024f845ad23e8547122454d8153d1f4ad
gurhank <gurhankocyigit@gmail.com>
2021-01-11 10:44:03 -0600

    day6_commits dropdown practices

--- src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/DropdownTasks.java
+++ src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/DropdownTasks.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -50,6 +51,15 @@ public class DropdownTasks {
         stateDropdown.selectByIndex(5);
 
         //6. Verify final selected option is California.
+        String expectedOption = "California";
+
+        //Finding currently selected option
+        String actualOption = stateDropdown.getFirstSelectedOption().getText();
+
+        //AssertEquals: expects args to compare
+
+        Assert.assertEquals(actualOption, expectedOption, "Final selected option is not as expected.");
+
     }
 
     @AfterMethod
