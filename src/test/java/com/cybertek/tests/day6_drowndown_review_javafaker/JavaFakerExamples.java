e895de3faf06321aa2b9ab8a1efcaaad6e6305b7
gurhank <gurhankocyigit@gmail.com>
2021-01-11 13:26:32 -0600

    day6_commits dropdown practices

---
+++ src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/JavaFakerExamples.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day6_drowndown_review_javafaker;
+
+import org.testng.annotations.Test;
+
+public class JavaFakerExamples {
+
+
+    @Test
+    public void java_faker_test1(){
+
+    }
+}
