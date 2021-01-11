820bae7394888a1cd53fb5fcb41dc6fe6bcbd825
gurhank <gurhankocyigit@gmail.com>
2021-01-11 15:10:33 -0600

    day6_commits web_order practices

--- src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/WebOrder_Practices.java
+++ src/test/java/com/cybertek/tests/day6_drowndown_review_javafaker/WebOrder_Practices.java
@@ -37,6 +37,20 @@ public class WebOrder_Practices {
 
     }
 
+    @Test
+    public void test2_create_order_with_java_faker(){
+        //6. Click on Order
+        //7. Select familyAlbum from product, set quantity to 2
+        //8. Click to “Calculate” button
+        //9. Fill address Info with JavaFaker
+        //• Generate: name, street, city, state, zip code
+        //10. Click on “visa” radio button
+        //11. Generate card number using JavaFaker
+        //12. Click on “Process”
+        //13.Verify success message “New order has been successfully added.
+    }
+
+
 
 
     @Test
