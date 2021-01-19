0d8ec1d6b38e34980122ca85f2fd4d7df6955d15
gurhank <gurhankocyigit@gmail.com>
2021-01-19 15:34:33 -0600

    day7_commits WINDOW practice

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/WindowHandlePractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/WindowHandlePractice.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,6 +21,11 @@ public class WindowHandlePractice {
         driver.get("http://practice.cybertekschool.com/windows");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void window_handling_test(){
 
     }
 
