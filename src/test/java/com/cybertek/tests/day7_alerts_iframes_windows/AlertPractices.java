a73029271cdcfbe31dd8b520fe0f27b9172fdea6
gurhank <gurhankocyigit@gmail.com>
2021-01-19 13:49:20 -0600

    day7_commits ALERTS practices

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/AlertPractices.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/AlertPractices.java
@@ -1,9 +1,11 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -28,19 +30,35 @@ public class AlertPractices {
     //TC #1: Information alert practice
 
     @Test
-    public void p1_information_alert_test(){
+    public void p1_information_alert_test() throws InterruptedException {
         //Locating the button for alert to click
         WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
 
-        //Clicking to the button for alert to be displayed
+        //3.Clicking to the button for alert to be displayed
         informationAlertButton.click();
+
+        Thread.sleep(2000);
+
+        //4. Click to OK button from the alert
+        //Switching driver's focus to alert
+        Alert alert = driver.switchTo().alert();
+
+        //Use 'alert' instance to click and close the alert
+        alert.accept();
 
         //5. Verify "You successfully clicked an alert" text is displayed.
 
+        WebElement resultText = driver.findElement(By.id("result"));
+
+        String actualText = resultText.getText();
+        String expectedText = "You successfuly clicked an alert";
+
+        Assert.assertTrue(resultText.isDisplayed());
+        Assert.assertEquals(actualText, expectedText);
 
 
-        //4. Click to OK button from the alert
-        //3. Click to "Click for JS Alert" button
+
+
 
     }
 
