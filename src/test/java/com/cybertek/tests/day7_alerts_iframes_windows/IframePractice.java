5db298347a41de534c36264030e775084b265841
gurhank <gurhankocyigit@gmail.com>
2021-01-19 14:27:11 -0600

    day7_commits IFRAME practice

---
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/IframePractice.java
@@ -0,0 +1,31 @@
+package com.cybertek.tests.day7_alerts_iframes_windows;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class IframePractice {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+
+        //2. Go to website: http://practice.cybertekschool.com/javascript_alerts
+        driver.get("http://practice.cybertekschool.com/iframe");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void teardownMethod() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.close();
+    }
+
+}
