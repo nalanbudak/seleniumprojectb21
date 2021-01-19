d054645b796c5f22893731ac79ea2b711e49cbb2
gurhank <gurhankocyigit@gmail.com>
2021-01-19 13:30:20 -0600

    day7_commits ALERTS practices

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/AlertPractices.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/AlertPractices.java
@@ -2,22 +2,31 @@ package com.cybertek.tests.day7_alerts_iframes_windows;
 
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
 public class AlertPractices {
+    WebDriver driver;
 
     @BeforeMethod
     public void setUpMethod(){
         //1. Open browser
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
 
         //2. Go to website: http://practice.cybertekschool.com/javascript_alerts
         driver.get("http://practice.cybertekschool.com/javascript_alerts");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+    }
+
+    //AfterMethod will wait 5 seconds before closing the browser
+    @AfterMethod
+    public void teardownMethod() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.close();
     }
 
     //TC #1: Information alert practice
