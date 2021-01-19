f59cb72f778adf432fdad8e0bde94f9654f0ccbb
gurhank <gurhankocyigit@gmail.com>
2021-01-19 14:32:05 -0600

    day7_commits IFRAME practice

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/IframePractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/IframePractice.java
@@ -1,9 +1,12 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,6 +24,17 @@ public class IframePractice {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
+
+    @Test
+    public void iframe_test(){
+
+        WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
+
+        // 4.Assert: "Your content goes here." Text is displayed.
+        // 5.Assert: "An iFrame containing the TinyMCEWYSIWYG Editor
+    }
+
+
 
     @AfterMethod
     public void teardownMethod() throws InterruptedException {
