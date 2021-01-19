72d6f8b7cb9271c64b04f19b4d76386ff7089c5d
gurhank <gurhankocyigit@gmail.com>
2021-01-19 15:33:57 -0600

    day7_commits WINDOW practice

---
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/WindowHandlePractice.java
@@ -0,0 +1,33 @@
+package com.cybertek.tests.day7_alerts_iframes_windows;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class WindowHandlePractice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+
+        //2. Go to website: http://practice.cybertekschool.com/javascript_alerts
+        driver.get("http://practice.cybertekschool.com/windows");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+
+    @AfterMethod
+    public void teardownMethod() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.close();
+    }
+
+}
