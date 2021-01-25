dd2e2ccfbc476bdeb89f9a2fab3414634a3fd7d3
gurhank <gurhankocyigit@gmail.com>
2021-01-25 11:37:19 -0600

    day8_commits

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
@@ -1,10 +1,32 @@
 package com.cybertek.tests.day8_testbase_properties_driver;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class WebTablePractices {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+
+        //2. Go to website: http://practice.cybertekschool.com/javascript_alerts
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete12%2fWebOrders%2fDefault.aspx");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
     @Test
+    public void verify_order_test(){
+
+    }
 
 
 }
