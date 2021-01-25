5a5df2ad5b71ee7b5c4fc605ad060bc5a2b924f9
gurhank <gurhankocyigit@gmail.com>
2021-01-25 14:03:16 -0600

    day8_commits

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/LearningProperties.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/LearningProperties.java
@@ -10,6 +10,9 @@ public class LearningProperties {
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
         System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
 
+        //properties are stored as key and value format
+        //
+
 
     }
 
