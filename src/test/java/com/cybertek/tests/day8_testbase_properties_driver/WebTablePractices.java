c577b12b641a49ced84854eb31deb20200822014
gurhank <gurhankocyigit@gmail.com>
2021-01-25 12:04:50 -0600

    day8_commits

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
@@ -10,22 +10,10 @@ import java.util.concurrent.TimeUnit;
 
 public class WebTablePractices {
 
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUpMethod(){
-        //1. Open browser
-        driver = WebDriverFactory.getDriver("chrome");
-
-        //2. Go to website: http://practice.cybertekschool.com/javascript_alerts
-        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete12%2fWebOrders%2fDefault.aspx");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    }
-
     @Test
     public void verify_order_test() throws InterruptedException {
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete12%2fWebOrders%2fDefault.aspx");
+
         WebOrderUtils.loginToSmartBear(driver);
 
         Thread.sleep(1000);
