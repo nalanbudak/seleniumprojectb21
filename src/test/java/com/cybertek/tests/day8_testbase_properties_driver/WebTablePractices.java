bf9a014b4eb45c3c9b6e5f2250a54adb9a2e9ee9
gurhank <gurhankocyigit@gmail.com>
2021-01-25 14:56:37 -0600

    day8_commits

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
@@ -5,11 +5,30 @@ import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.WebOrderUtils;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class WebTablePractices extends TestBase {
 
     @Test
-    public void verify_order_test() throws InterruptedException {
-        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete12%2fWebOrders%2fDefault.aspx");
+    public void verify_order_test() throws InterruptedException, IOException {
+        //#1- Create Properties class object
+        Properties properties = new Properties();
+
+        //#2- Open the file in JVM Memory and pass the path of the file
+        String path = "configuration.properties";
+        //For windows > copy path > copy path from content root
+
+        FileInputStream file = new FileInputStream(path);
+
+        //#3- Load the opened file into the Properties object.
+
+        properties.load(file);
+
+        String url = properties.getProperty("webOrderUrl");
+
+        driver.get(url);
 
         WebOrderUtils.loginToSmartBear(driver);
 
