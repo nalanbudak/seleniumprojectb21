c7e185caeb8162db5f97e57d5624acc4854162e5
gurhank <gurhankocyigit@gmail.com>
2021-02-02 11:06:43 -0600

    day9_commits Driver utils

--- src/test/java/com/cybertek/tests/day9_driver_actions_uploads/DriverUtilsPractice.java
+++ src/test/java/com/cybertek/tests/day9_driver_actions_uploads/DriverUtilsPractice.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day9_driver_actions_uploads;
 
+import com.cybertek.utilities.Driver;
 import org.testng.annotations.Test;
 
 public class DriverUtilsPractice {
@@ -8,6 +9,15 @@ public class DriverUtilsPractice {
     public void simple_google_search_test(){
 
         //Go to google.com
+
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.get("https://www.google.com");
+
+        Driver.getDriver().get("https://www.google.com");
+
         //Search for a value
         //Assert title contains the value
 
