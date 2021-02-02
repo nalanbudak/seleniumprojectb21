0a08b25180689c85decac170035578ca1da77d4d
gurhank <gurhankocyigit@gmail.com>
2021-02-02 15:49:42 -0600

    day9_commits uploads

--- src/test/java/com/cybertek/tests/day9_driver_actions_uploads/HoverPractice.java
+++ src/test/java/com/cybertek/tests/day9_driver_actions_uploads/HoverPractice.java
@@ -1,8 +1,11 @@
 package com.cybertek.tests.day9_driver_actions_uploads;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HoverPractice {
@@ -24,13 +27,27 @@ public class HoverPractice {
         WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
         WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
 
+        BrowserUtils.sleep(2);
+
+        //We MUST create object of Actions class' object to be able to use its method
+        Actions actions = new Actions(Driver.getDriver());
 
         //2. Hover over to first image
+        actions.moveToElement(img1).perform();
+        //actions.perform();
+
         //3. Assert:
         //a. “name: user1” is displayed
+        Assert.assertTrue(user1.isDisplayed(), "User1 is not displayed. FAILED!!!");
+        BrowserUtils.sleep(1);
+
         //4. Hover over to second image
+        actions.moveToElement(img2).perform();
+
         //5. Assert:
         //a. “name: user2” is displayed
+        Assert.assertTrue(user2.isDisplayed(), "User2 is not displayed. FAILED!!!");
+
         //6. Hover over to third image
         //7. Confirm:
         //a. “name: user3” is displayed
