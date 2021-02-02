31a99f91e9150e9b7f5f13fabe63785a5e700a7a
gurhank <gurhankocyigit@gmail.com>
2021-02-02 11:04:20 -0600

    day9_commits Driver utils

--- src/test/java/com/cybertek/tests/day9_driver_actions_uploads/DriverUtilsPractice.java
+++ src/test/java/com/cybertek/tests/day9_driver_actions_uploads/DriverUtilsPractice.java
@@ -1,4 +1,19 @@
 package com.cybertek.tests.day9_driver_actions_uploads;
 
+import org.testng.annotations.Test;
+
 public class DriverUtilsPractice {
+
+    @Test
+    public void simple_google_search_test(){
+
+        //Go to google.com
+        //Search for a value
+        //Assert title contains the value
+
+
+
+    }
+
+
 }
