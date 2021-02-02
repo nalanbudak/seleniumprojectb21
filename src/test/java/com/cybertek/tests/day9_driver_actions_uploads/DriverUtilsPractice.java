a38d37f839469f841ffe8527d4a3be12d0564127
gurhank <gurhankocyigit@gmail.com>
2021-02-02 11:19:27 -0600

    day9_commits Driver utils

--- src/test/java/com/cybertek/tests/day9_driver_actions_uploads/DriverUtilsPractice.java
+++ src/test/java/com/cybertek/tests/day9_driver_actions_uploads/DriverUtilsPractice.java
@@ -1,6 +1,9 @@
 package com.cybertek.tests.day9_driver_actions_uploads;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class DriverUtilsPractice {
@@ -16,9 +19,16 @@ public class DriverUtilsPractice {
 //        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //        driver.get("https://www.google.com");
 
+        //Driver.getDriver() = driver
         Driver.getDriver().get("https://www.google.com");
 
         //Search for a value
+        WebElement searchBox = Driver.getDriver().findElement(By.name("q"));
+
+        String searchValue = "wooden spoon";
+
+        searchBox.sendKeys(searchValue + Keys.ENTER);
+
         //Assert title contains the value
 
 
