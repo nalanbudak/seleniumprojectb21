bcb3c32b442c1b802b0916cf998774f899922767
gurhank <gurhankocyigit@gmail.com>
2021-02-02 15:41:24 -0600

    day9_commits uploads

--- src/test/java/com/cybertek/tests/day9_driver_actions_uploads/HoverPractice.java
+++ src/test/java/com/cybertek/tests/day9_driver_actions_uploads/HoverPractice.java
@@ -1,6 +1,8 @@
 package com.cybertek.tests.day9_driver_actions_uploads;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class HoverPractice {
@@ -9,6 +11,19 @@ public class HoverPractice {
     public void hover_test(){
         //1. Go to http://practice.cybertekschool.com/hovers
         Driver.getDriver().get("http://practice.cybertekschool.com/hovers");
+
+        //locating all the web elements needed
+
+        //locating all the images from the page
+        WebElement img1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement img2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement img3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+        //locating all the "user" texts from the page for "isDisplayed" verification
+        WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user1']"));
+        WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
+        WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
+
 
         //2. Hover over to first image
         //3. Assert:
