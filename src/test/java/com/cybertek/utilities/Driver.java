e3119c88899a669c564894db8b1b788d94ff4a34
gurhank <gurhankocyigit@gmail.com>
2021-02-02 10:33:40 -0600

    day9_commits Driver utils

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -1,12 +1,36 @@
 package com.cybertek.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
 
     private Driver(){}
 
     private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        if (driver == null){
+
+            String browser = ConfigurationReader.getProperty("browser");
+
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+
+
+            }
+
+
+
+        }
+
+
+    }
 
 
 
