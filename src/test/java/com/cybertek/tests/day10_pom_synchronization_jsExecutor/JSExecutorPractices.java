51794913ca17851e8bb2f7618d0a9f13c05e67ec
gurhank <gurhankocyigit@gmail.com>
2021-02-08 11:27:44 -0600

    day10_commits jsexecutor practices

--- src/test/java/com/cybertek/tests/day10_pom_synchronization_jsExecutor/JSExecutorPractices.java
+++ src/test/java/com/cybertek/tests/day10_pom_synchronization_jsExecutor/JSExecutorPractices.java
@@ -1,6 +1,7 @@
 package com.cybertek.tests.day10_pom_synchronization_jsExecutor;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class JSExecutorPractices {
@@ -11,6 +12,11 @@ public class JSExecutorPractices {
         //Get the page
         Driver.getDriver().get("http://practice.cybertekschool.com/infinite_scroll");
 
+        //Create instance of JSExecutor and cast our driver type to it
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //Use "js" to reach JavascriptExecutor methods
+        js.executeScript("window.scrollBy(0, 750)");
 
 
     }
