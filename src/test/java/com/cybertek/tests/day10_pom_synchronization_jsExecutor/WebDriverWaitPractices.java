ad676d89b7f5a5f77ba0798cb2f306b0f2e52a89
gurhank <gurhankocyigit@gmail.com>
2021-02-08 15:53:14 -0600

    day10_commits WebDriverWait practice

--- src/test/java/com/cybertek/tests/day10_pom_synchronization_jsExecutor/WebDriverWaitPractices.java
+++ src/test/java/com/cybertek/tests/day10_pom_synchronization_jsExecutor/WebDriverWaitPractices.java
@@ -2,6 +2,8 @@ package com.cybertek.tests.day10_pom_synchronization_jsExecutor;
 
 import com.cybertek.pages.DynamicLoad7Page;
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,6 +16,13 @@ public class WebDriverWaitPractices {
         Driver.getDriver().get("http://practice.cybertekschool.com/dynamic_loading/7");
 
         // 2.Wait until title is "Dynamic title"
+
+        //#1- create WebDriverWait instance
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
+        //2- create explicit wait condition. Create expected condition on title.
+        wait.until(ExpectedConditions.titleIs("Dynamic title"));
+
 
         DynamicLoad7Page dynamicLoad7Page = new DynamicLoad7Page();
 
