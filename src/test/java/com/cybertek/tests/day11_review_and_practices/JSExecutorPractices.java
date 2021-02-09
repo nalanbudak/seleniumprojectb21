1e9e53f7e3c43e40937ee675d60ee1e7d862ddb1
gurhank <gurhankocyigit@gmail.com>
2021-02-09 11:37:28 -0600

    day11_commits practices

--- src/test/java/com/cybertek/tests/day11_review_and_practices/JSExecutorPractices.java
+++ src/test/java/com/cybertek/tests/day11_review_and_practices/JSExecutorPractices.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.day11_review_and_practices;
 
+import com.cybertek.pages.SignUpPage;
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class JSExecutorPractices {
@@ -8,7 +11,18 @@ public class JSExecutorPractices {
     @Test
     public void fill_form_using_jsexecutor(){
 
-        //
+        //getting the page
+        Driver.getDriver().get("http://practice.cybertekschool.com/sign_up");
+
+        //Create instance of JSExecutor and cast our WebDriver type to it
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //Creating the object to be able to use WebElements from it
+        SignUpPage signUpPage = new SignUpPage();
+
+        //Use .setAttribute function from JavaScript to sendKeys
+        js.executeScript("arguments[0].setAttribute('value', 'jane doe')", signUpPage.inputFullName );
+
 
 
     }
