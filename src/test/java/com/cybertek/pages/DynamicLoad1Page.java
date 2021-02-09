c2748fe308b9b2d54166a86655b26f2a2054ff2b
gurhank <gurhankocyigit@gmail.com>
2021-02-09 10:49:44 -0600

    day11_commits practices

--- src/test/java/com/cybertek/pages/DynamicLoad1Page.java
+++ src/test/java/com/cybertek/pages/DynamicLoad1Page.java
@@ -14,6 +14,22 @@ public class DynamicLoad1Page {
     @FindBy(xpath = "//button[.='Start']")
     public WebElement startButton;
 
+    @FindBy(id = "loading")
+    public WebElement loadingBar;
+
+    @FindBy(id = "username")
+    public WebElement inputUsername;
+
+    @FindBy(id = "pwd")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//button[.='Submit']")
+    public WebElement submitButton;
+
+    @FindBy(id = "flash")
+    public WebElement errorMessage;
+
+
 
 
 }
