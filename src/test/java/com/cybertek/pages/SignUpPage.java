aa1a31a0c7478d7d6d52d50c685a5e05589053de
gurhank <gurhankocyigit@gmail.com>
2021-02-09 11:31:23 -0600

    day11_commits practices

--- src/test/java/com/cybertek/pages/SignUpPage.java
+++ src/test/java/com/cybertek/pages/SignUpPage.java
@@ -4,6 +4,8 @@ package com.cybertek.pages;
 // http://practice.cybertekschool.com/sign_up
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SignUpPage {
@@ -12,7 +14,14 @@ public class SignUpPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(name="full_name")
+    public WebElement inputFullName;
 
+    @FindBy(name="email")
+    public WebElement inputEmail;
+
+    @FindBy(name="wooden_spoon")
+    public WebElement signUpButton;
 
 
 }
